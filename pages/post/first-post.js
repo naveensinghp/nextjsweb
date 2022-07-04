24ee3c512ac09803b4ff46f17040704a9ec9d049
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-04 23:37:19 +0530

    no message

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,17 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import Script from 'next/script';
 
 
 export default function FirstPost(){
     return (
         <>
         <Head>
-         <title>First Post</title>
+         <title>Learning</title>
+        
         </Head>
-         <h1>First Post</h1>
-        <h2><Link href="/"><a>Back Home</a></Link></h2>
-        {/* <img src="/image/cycling.png" alt="cycling" /> */}
+         <h1>Naveen SinghPost</h1>
+         <h2>Let's Start Project Together</h2>
+         <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload"
+            onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}/>
+        <h2>
+            <Link href="/"><a>Back Home</a></Link>
+        </h2>
         <Image src="/image/cycling.png" width={30} height={30} />
         
         </>
