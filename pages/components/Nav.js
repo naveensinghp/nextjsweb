0ee680523de4d4ec5c6f35a346cfa1f894603276
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-16 01:05:07 +0530

    Responsive Navbar

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,7 +1,28 @@
-export default function About(){
+// import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+
+
+const Navbar = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <header className="primary-header flex"> 
+         <button className="mobile-nav-toggle" aria-control="primary-navigation" aria-expand="false">
+         <span className="sr-only">
+            Menu
+         </span> 
+         </button>
+         
+            <nav>
+                <ul className="primary-navigation flex" id="primary-navigation">
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Skill</li>
+                    <li>Blog</li>
+                </ul>
+            </nav>
+       </header>
     );
 }
+
+export default Navbar
