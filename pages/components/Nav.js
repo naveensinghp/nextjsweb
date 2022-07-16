6f6437250177d5e1d16a1955d86df7c9a71e13eb
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-17 01:10:14 +0530

    no message

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,22 +1,34 @@
 // import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import { AiOutlineMenu } from 'react-icons/ai';
+import Script from 'next/script'
+
 
 
 
 const Navbar = () => {
+
+    function navStyle(){
+
+        const primaryNav = document.querySelector('primary-navigation');
+        const navToggle = document.querySelector('mobile-nav-toggle');
+        console.log(primaryNav);
+        if(primaryNav){
+           navToggle.addEventListener('click',() => {
+            const visibility = primaryNav.getAttribute('data-visible');
+        }) 
+        }
+        
+    }
     return (
         <header className="primary-header flex"> 
-        
-         <button className="mobile-nav-toggle">
+         <div className="mobile-nav-toggle">
          <span className="sr-only">
-            <AiOutlineMenu/>
+            <AiOutlineMenu size={35}/>
          </span> 
-         </button>
-         
-     
+         </div>
             <nav>
-                <ul className="primary-navigation flex" id="primary-navigation">
+                <ul className="primary-navigation flex" id="primary-navigation" data-visible="false">
                     <li>Home</li>
                     <li>Projects</li>
                     <li>Skill</li>
