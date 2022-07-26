0484026fb06245abb4458a94e20056ca016e19ac
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-26 11:40:37 +0530

    no message

---
+++ pages/components/Main.js
@@ -0,0 +1,12 @@
+const MainContent = () => {
+    return (
+        <div className="nameintro">
+            <div>Hello!</div>
+            <div>I'm NaveenSingh</div>
+            <div>Front end Developer</div>
+            <div>UI/UX Designer</div>
+        </div>
+    );
+}
+
+export default MainContent
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 import Nav from './components/Nav'
+import MainContent from './components/Main'
 
 
 export default function Home(){
   return (
     <>
       <Nav/>
+      <MainContent/>
     </>
   );
 }
