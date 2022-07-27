bea37444fa621bbc3b98739972e947bacc7a7bfc
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-27 23:06:06 +0530

    no message

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -3,47 +3,53 @@ import Link from 'next/link';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { MdOutlineLightMode } from 'react-icons/md';
 import { AiFillSound,AiFillGithub,AiOutlineTwitter } from 'react-icons/ai';
+import React from 'react'
+
 
 
 
 
 const Navbar = () => {
-    function navStyle(){
-        const primaryNav = document.querySelector('primary-navigation');
-        const navToggle = document.querySelector('mobile-nav-toggle');
-        console.log(primaryNav);
-        if(primaryNav){
-           navToggle.addEventListener('click',() => {
-            const visibility = primaryNav.getAttribute('data-visible');
-        }) 
-        }
-        
+    // function navStyle(){
+    //     const primaryNav = document.querySelector('primary-navigation');
+    //     const navToggle = document.querySelector('mobile-nav-toggle');
+    //     console.log(primaryNav);
+    //     if(primaryNav){
+    //        navToggle.addEventListener('click',() => {
+    //         const visibility = primaryNav.getAttribute('data-visible');
+    //     }) 
+    //     }
+    // }
+    const clickData = () => {
+        alert('clicked');
     }
     return (
         <header className="primary-header flex"> 
-         <div className="mobile-nav-toggle">
-         <span className="sr-only">
-            <AiOutlineMenu size={35}/>
-         </span> 
-         </div>
-            <ul className='left-navigation flex'>
+            <div className="mobile-nav-toggle">
+                <span className="sr-only">
+                    <AiOutlineMenu onClick={clickData} size={35}/>
+                </span> 
+            </div>
+            <div className="nametitle">
+                <h1>NaveenSingh</h1>
+            </div>
+            {/* <ul className='left-navigation flex'>
                 <li><MdOutlineLightMode size={28} /></li>
                 <li><AiFillSound size={28} /> </li>
                 <li><AiFillGithub size={28} /> </li>
                 <li><AiOutlineTwitter size={28} /> </li>
-            </ul>
-            <div class="flex-gap">
-                <h1>NaveenSingh</h1>
-            </div>
+            </ul> */}
             <nav>
                 <ul className="primary-navigation flex" id="primary-navigation" data-visible="false">
                     <li>Home</li>
                     <li>Projects</li>
                     <li>My Skills </li>
                     <li>Blog</li>
+                    <li><MdOutlineLightMode size={20} /></li>
+                    <li><AiFillSound size={22} /> </li>
                 </ul>
             </nav>
-       </header>
+        </header>
     );
 }
 
