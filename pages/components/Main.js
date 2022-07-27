ca96f04e7b9f69efc86e02e222bf70f228c474de
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-28 00:40:04 +0530

    no message

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,10 +1,19 @@
+import { MdOutlineLightMode } from 'react-icons/md';
+import { AiFillSound,AiFillGithub,AiOutlineTwitter,AiFillLinkedin } from 'react-icons/ai';
+
 const MainContent = () => {
     return (
         <div className="nameintro">
-            <div>Hello!</div>
-            <div>I'm NaveenSingh</div>
-            <div>Front end Developer</div>
-            <div>UI/UX Designer</div>
+            <div>Hello! I'm <span className="tetsName">
+                NaveenSingh</span>
+            </div>
+            <div>Front End Developer</div>
+            {/* <div>UI/UX Designer</div> */}
+            <ul className='socialmedia-navigation flex'>
+                <li><AiOutlineTwitter size={28} /> </li>
+                <li><AiFillGithub size={28} /> </li>
+                <li><AiFillLinkedin size={28} /></li>
+            </ul>
         </div>
     );
 }
