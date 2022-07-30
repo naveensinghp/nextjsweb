e3fd0f8234afeb86ed6d10f16ec580b9874c63d3
NaveenSingh <naveensingh.mtp@gmail.com>
2022-07-30 23:00:37 +0530

    no message

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -3,24 +3,26 @@ import Image from 'next/image'
 
 const MainContent = () => {
     return (
-        <div className="nameintro">
-            <div>Hello! I'm <span className="tetsName">
-                NaveenSingh</span>
+        <div className="">
+            <div className='banner-right'>
+                <div className="nameintro">
+                    <div>Hello! I'm <span className="tetsName">
+                        NaveenSingh</span>
+                    </div>
+                    <div>
+                        Front End Developer
+                    </div>
+                    <ul className='socialmedia-navigation flex'>
+                        <li><AiOutlineTwitter size={28} /> </li>
+                        <li><AiFillGithub size={28} /> </li>
+                        <li><AiFillLinkedin size={28} /></li>
+                    </ul>
+                <div>
             </div>
-            <div>Front End Developer</div>
-            {/* <div>UI/UX Designer</div> */}
-            <ul className='socialmedia-navigation flex'>
-                <li><AiOutlineTwitter size={28} /> </li>
-                <li><AiFillGithub size={28} /> </li>
-                <li><AiFillLinkedin size={28} /></li>
-            </ul>
-            <div>
-            <span className=''>
-                {/* <Image src="/assets/images/banner_test.png" alt="Vercel Logo" width={1092} height={842} /> */}
-                <img src='/assets/images/banner_test.png' width={500}/>
-            </span>
             </div>
+            <img src='/assets/images/banner_test.png' width={500}/>
         </div>
+    </div>
     );
 }
 
