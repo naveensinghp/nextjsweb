0f2b472c224aaf55b0c6c63c25e534888030c646
NaveenSingh <naveensingh.mtp@gmail.com>
2022-08-10 22:42:54 +0530

    no message

---
+++ pages/main/index.js
@@ -0,0 +1,31 @@
+import { AiFillGithub,AiOutlineTwitter,AiFillLinkedin } from 'react-icons/ai';
+import Image from 'next/image'
+
+const MainContent = () => {
+    return (
+        <div className="">
+            <div className='banner-right'>
+                <div className="nameintro">
+                    <div>Hello! I'm <span className="tetsName">
+                        NaveenSingh</span>
+                       
+                    </div>
+                    <p>
+                        Front End Developer
+                    </p>
+                    
+                    <ul className='socialmedia-navigation flex'>
+                        <li><AiOutlineTwitter size={28} /> </li>
+                        <li><AiFillGithub size={28} /> </li>
+                        <li><AiFillLinkedin size={28} /></li>
+                    </ul>
+                <div>
+            </div>
+            </div>
+            <img src='/assets/images/banner_test.png' width={500}/>
+        </div>
+    </div>
+    );
+}
+
+export default MainContent
