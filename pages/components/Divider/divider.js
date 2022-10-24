e504f4d727b5a5402852279048b97e0541e078d0
NaveenSingh <naveensingh.mtp@gmail.com>
2022-10-24 08:00:48 +0530

    no message

--- pages/components/Divider/divider.js
+++ pages/components/Divider/divider.js
@@ -3,8 +3,8 @@
 
 const Divider =() => {
     return<>
-            <svg  style={{ margin: '70px auto' }} width="100" height="50"><path d="M 0,25 Q 5,30 10,25 Q 15,20 20,25 Q 25,30 30,25 Q 35,20 40,25 Q 45,30 50,25 Q 55,20 60,25 Q 65,30 70,25 Q 75,20 80,25 Q 85,30 90,25 Q 95,20 100,25" fill="none" stroke-linecap="round" stroke="#D500F9" stroke-width="2"></path>
-            </svg>
+            {/* <svg  style={{ margin: '70px auto' }} width="100" height="50"><path d="M 0,25 Q 5,30 10,25 Q 15,20 20,25 Q 25,30 30,25 Q 35,20 40,25 Q 45,30 50,25 Q 55,20 60,25 Q 65,30 70,25 Q 75,20 80,25 Q 85,30 90,25 Q 95,20 100,25" fill="none" stroke-linecap="round" stroke="#D500F9" stroke-width="2"></path>
+            </svg> */}
         </>
 }
 
